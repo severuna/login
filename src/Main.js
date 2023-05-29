584897016c5059d9b2efaa814df087720c71bb7c
Severuna <oxxxyfomenkova@gmail.com>
2023-05-29 22:32:28 +0300

    clear proj

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className='main column'>
+            <h1 className='title'>Init title</h1>
+        </div>
+    );
+};
+
+export default Main;
